9c16abcf469e230c33fbb2a9eb34b2a664ea1425
uchikakoi-eishou <kakoi.0501souta@gmail.com>
2020-06-21 00:28:37 +0900

    デザイン中

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { useForm } from 'react-hook-form';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import './css/index.css';
+import './css/index.css';
 
-function App() {
+function Form() {
     const { register, handleSubmit, errors } = useForm();
     const onSubmit = (data) => {
         console.log(data);
@@ -13,33 +13,33 @@ function App() {
 
     return (
         <div className="container">
-            <header>
-                <h1 className="h2 text-center pt-2 pb-2 text-de">Form</h1>
+            <header className="border-bottom-dark">
+                <h1 className="h2 text-center pt-2 pb-2">Form</h1>
             </header>
             <main>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="row">
+                    <div className="form-group">
                         <label htmlFor="fullName">お名前</label>
                         {errors.fullName && errors.fullName.type === 'required' && <p>入力されていません。</p>}
-                        <input className="inputInput" type="text" id="fullName" name="fullName" ref={register({ required: true, minLength: 1})}/>
+                        <input className="form-control" type="text" id="fullName" name="fullName" ref={register({ required: true, minLength: 1})}/>
                     </div>
-                    <div className="row">
+                    <div className="form-group">
                         <label htmlFor="mail">メールアドレス</label>
                         {errors.mail && <p>入力されていません。</p>}
-                        <input className="inputInput" type="mail" id="mail" name="mail" ref={register({ required: true, minLength: 1})}/>
+                        <input className="form-control" type="mail" id="mail" name="mail" ref={register({ required: true, minLength: 1})}/>
                     </div>
-                    <div className="row">
+                    <div className="form-group">
                         <label htmlFor="content">内容</label>
                         {errors.content && <p>入力されていません。</p>}
-                        <textarea className="inputInput" type="text" id="content" name="content" ref={register({ required: true, minLength: 1})}/>
+                        <textarea className="form-control" type="text" id="content" name="content" ref={register({ required: true, minLength: 1})}/>
                     </div>
-                    <div className="row center">
-                        <input type="checkbox" id="privacy" name="privacy" ref={register({ required: true})}/>
-                        <label className="labelPrivacy" htmlFor="privacy">プライバシーに同意する</label>
+                    <div className="form-group">
+                        <input className="form-check-input" type="checkbox" id="privacy" name="privacy" ref={register({ required: true})}/>
+                        <label className="form-check-label" htmlFor="privacy">プライバシーに同意する</label>
                     </div>
-                    <div className="row center">
+                    <div className="form-group">
                         {errors.privacy && <p>チェックがされていません。</p>}
-                        <input type="submit" value="送信" name="btn" ref={register({ required: true})}/>
+                        <input className="btn btn-primary" type="submit" value="送信" name="btn" ref={register({ required: true})}/>
                     </div>
                 </form>
             </main>
@@ -48,4 +48,4 @@ function App() {
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Form />, rootElement);
